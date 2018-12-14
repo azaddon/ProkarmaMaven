171f17327480c5b8e4644c4f5c2c2c53a22227ac
azad khan <incrediblefighterzz@gmail.com>
2018-12-14 17:05:30 +0530

    changes made successfully

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,10 @@ public class Greeter {
   }
 
  /**
-  * @param this is javadoc comment
-  * @return
+  * @param someone the name of person
+  * @return greeting string
   */
-  public final String greet( final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
