ea65f86515bab4099e2abddb0f92b352baeac809
azad khan <incrediblefighterzz@gmail.com>
2018-12-14 16:40:10 +0530

    checkstyle warning fixes done at 4:40

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,9 @@ public class Greeter {
 
   }
 
-  /*
-   * this is javadoc comment
-   * 
-   */
- 
+ /**
+  * @paramater this is javadoc comment
+  */
   public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -2,5 +2,7 @@
  * @param this is package name
  * @return this is return
 */
-
+/**
+ * @param this is comment
+ */
 package com.example;
